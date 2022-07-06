27aa12e42c879219c43f9330de7d68414b5e85f3
Natalya Blanco <natalyablanco@gmail.com>
2022-07-06 23:37:35 +0200

    changes api_key

---
+++ app/src/main/java/com/example/bonprixchallenge/Secrets.kt
@@ -0,0 +1,4 @@
+package com.example.bonprixchallenge
+
+class Secrets {
+}
---
+++ app/src/main/java/com/example/bonprixchallenge/network/RetrofitService.kt
@@ -0,0 +1,30 @@
+package com.example.bonprixchallenge.network
+
+import com.example.bonprixchallenge.Secrets
+import com.example.bonprixchallenge.domain.Categories
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface RetrofitService {
+
+    @Headers("x-api-key:" + Secrets.API_KEY)
+    @GET("navigation")
+    suspend fun getNavigation(): Response<Categories>
+
+    companion object {
+        var retrofitService: RetrofitService? = null
+        fun getInstance(): RetrofitService {
+            if (retrofitService == null) {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("https://codechallenge.mobilelab.io/v1/bonprix/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                retrofitService = retrofit.create(RetrofitService::class.java)
+            }
+            return retrofitService!!
+        }
+    }
+}
